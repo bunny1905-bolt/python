dcf191168cf85599b63f122c49f7982fb87dda79
Chintakayala Venkata Abhishek <156206564+bunny1905-bolt@users.noreply.github.com>
2024-01-10 19:33:21 +0530

    Add files via upload

---
+++ username and password.py
@@ -0,0 +1,7 @@
+username=str(input("enter the username:"))
+password=str(input("enter the password:"))
+
+if len(username)>=5 and len(password)>=6:
+    print("Username and password are under cirita")
+else:
+    print("Username and password are not under cirita")
