037c6ff370bedc399fb3270ec11442af8371c76a
Chintakayala Venkata Abhishek <156206564+bunny1905-bolt@users.noreply.github.com>
2024-01-10 19:39:33 +0530

    Add files via upload

---
+++ reverse of a number.py
@@ -0,0 +1,9 @@
+#reverse of a number
+num=int(input("Enter the number:"))
+reversed_num = 0
+
+while num > 0:
+    digit = num%10
+    reversed_num = (reversed_num * 10) + digit
+    num = num//10
+    print(reversed_num)
