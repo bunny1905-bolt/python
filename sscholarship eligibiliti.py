86205f5a8d04cb18a4cebb66b9c9f4a3ff47fb9c
Chintakayala Venkata Abhishek <156206564+bunny1905-bolt@users.noreply.github.com>
2024-01-10 19:37:49 +0530

    Add files via upload

---
+++ sscholarship eligibiliti.py
@@ -0,0 +1,7 @@
+#SCHOLARSHIP ELIGIBILITY
+GPA = float(input("ENTER YOUR GPA"))
+activities_participation=input ("did you partcipated in my activities(yes/no)")
+if GPA>=3.5 and activities_participation == "yes":
+    print("you are eligible")
+else:
+    print("you are not eligible")
